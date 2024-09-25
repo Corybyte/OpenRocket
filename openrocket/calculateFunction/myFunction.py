5978a237b8b7a531f5849d1c4511fa7226d94606
destiny <867046011@qq.com>
2024-09-25 20:26:50 +0800

    destiny: 增加了数据点边界判断

--- openrocket/calculateFunction/myFunction.py
+++ openrocket/calculateFunction/myFunction.py
@@ -1,13 +1,18 @@
 import math
+import random
+
+
 def check_continuous(low, high):
     for i in range(len(low) - 1):
         if high[i] != low[i + 1]:
             return False
     return True
+
+
 def functions() -> tuple[list[float], list[float]]:
     # 定义字符串形式的函数列表
     # 定义字符串形式的函数列表
-    functions = ["x", "-x", "math.sin(x)"]
+    functions = ["x", "x*x", "x*x*x"]
 
     # low 和 high 列表分别代表 x 的取值范围
     low = [0, 10, 20]  # 每个函数对应的 x 的最小值
@@ -15,7 +20,7 @@ def functions() -> tuple[list[float], list[float]]:
 
     if low[0] is not 0:
         print("起始时间不为0")
-    if check_continuous(low, high):
+    if not check_continuous(low, high):
         print("区间不连续")
 
     # 计算值, 假设我们在每个区间中取 5 个点进行计算
@@ -28,12 +33,12 @@ def functions() -> tuple[list[float], list[float]]:
         x_values = [low[i] + j * (high[i] - low[i]) / (num_points - 1) for j in range(num_points)]
         # 计算当前函数在区间内的 y 值
         y_values = [eval(f) for x in x_values]
-
-        # 将 x 和 y 的值分别添加到各自的列表中
+        # 处理边界问题,防止重复
+        if len(x_list) is not 0 and x_list[-1] == x_values[0]:
+            x_values[0] += round(random.uniform(0.0001, 0.00000001), 6)
+        print(x_values[0])
         x_list.extend(x_values)
         y_list.extend(y_values)
     print(x_list)
     print(y_list)
     return x_list, y_list
-functions()
-
--- openrocket/calculatePoint/myPoint.py
+++ openrocket/calculatePoint/myPoint.py
@@ -1,4 +1,4 @@
 def point() -> tuple[list[float], list[float]]:
-    xlist = [1.0, 2.0, 3.0,4.0]
-    ylist = [1.0, 2.0, 3.0,4.0]
+    xlist = [0.0, 2.0, 3.0, 4.0]
+    ylist = [0.0, 2.0, 3.0, 4.0]
     return xlist, ylist
