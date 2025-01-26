7ed6f0d8f3f2211b93a1ea689d6e8d60bacc5aa4
destiny <867046011@qq.com>
2025-01-26 18:55:06 +0800

    关闭热部署

--- main.py
+++ main.py
@@ -1152,4 +1152,4 @@ def calculateComponentNonAxialForces_api():
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=8080,debug=True, threaded=True)
+    app.run(host="127.0.0.1", port=8080,debug=False, threaded=True)
